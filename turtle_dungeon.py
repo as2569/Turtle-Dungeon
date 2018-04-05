ce7d626b46e395aee9dc27797e7164b003a0075f
Adam Spryszynski <as2569@njit.edu>
2018-04-05 12:25:00 -0400

    added 'triple split' rule for dead ends

--- turtle_dungeon.py
+++ turtle_dungeon.py
@@ -54,43 +54,55 @@ class Manager():
         manager.roomCount += 1
         manager.path = manager.path + 'f'
 
-def DecideDirection(t):
-    x = random.random()
-    if x <= manager.contChance:
-        return
-    else:
-        y = random.randrange(0, 2)
-        if y == 0:
-            t.right(90)
-            manager.path = manager.path + 'r'
+    def DecideDirection(self, t):
+        x = random.random()
+        if x <= self.contChance:
+            return
         else:
-            t.left(90)
-            manager.path = manager.path + 'l'
+            y = random.randrange(0, 3)
+            if y == 0:
+                t.right(90)
+                self.path = self.path + 'r'
+            elif y == 1:
+                t.left(90)
+                self.path = self.path + 'l'
+            else:
+                t.left(180)
+                self.path = self.path + 'u'
 
-def RandomDirection(t):
-    x = random.random()
-    if x <= 0.25:
-        t.right(90)
-    elif x <= 0.50:
-        t.left(90)
-    elif x <= 0.75:
-        t.left(180)
-    else:
-        return
-
-def DrawFromInput(t, in_str):
-    print("Drawing from input")
-    print(in_str)
-    manager.DrawInitialRoom(t)
-    for ch in in_str:
-        if ch == 'f':
-            manager.DrawRoom(t)
-        elif ch == 'l':
-            t.left(90)
-        elif ch == 'r':
+    def RandomWalk(self, t):
+        while(manager.roomCount < 50):
+            rand = random.randrange(4, 7)
+            for x in range(rand):
+                self.DrawRoom(alex)
+            self.DecideDirection(alex)
+        
+    def RandomDirection(self, t):
+        x = random.random()
+        if x <= 0.25:
             t.right(90)
+        elif x <= 0.50:
+            t.left(90)
+        elif x <= 0.75:
+            t.left(180)
         else:
-            print("Unexpected input")
+            return
+
+    def DrawFromInput(self, t, in_str):
+        print("Drawing from input")
+        print(in_str)
+        self.DrawInitialRoom(t)
+        for ch in in_str:
+            if ch == 'f':
+                self.DrawRoom(t)
+            elif ch == 'l':
+                t.left(90)
+            elif ch == 'r':
+                t.right(90)
+            elif ch == 'u':
+                t.left(180)
+            else:
+                print("Unexpected input")
 
 def Reset(this_screen, this_turtle):
     this_screen.reset()
@@ -104,27 +116,23 @@ def SavePath():
     print(manager.path)
     manager.pathList.append(manager.path)
     manager.path = ''
-    
-manager = Manager(0.8)
 
+manager = Manager(0.5)
+l_sys = L_system()
 turtle.setup(1200, 800)
 win = turtle.Screen()
-win.screensize(3000, 3000)
+win.screensize(2000, 2000)
 alex = turtle.Turtle()
 
 Reset(win, alex)
 
 manager.DrawInitialRoom(alex)
-while(manager.roomCount < 25):
-    manager.DrawRoom(alex)
-    DecideDirection(alex)
-    #RandomDirection(alex)
+manager.RandomWalk(alex)
 
 SavePath()
 Reset(win, alex)
 
-l_sys = L_system()
 iteration = l_sys.createSystem(1, manager.pathList[0])
 manager.pathList.append(iteration)
-DrawFromInput(alex, manager.pathList[1])
+manager.DrawFromInput(alex, manager.pathList[1])
 
